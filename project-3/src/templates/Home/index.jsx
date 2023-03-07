b9ff27e5262668e04216f50c1d893be7c2fe8dfa
Fagenrsro <fagner.hg40@gmail.com>
2023-03-07 20:02:05 -0300

    useFetch custom hook, utilizando

--- project-3/src/templates/Home/index.jsx
+++ project-3/src/templates/Home/index.jsx
@@ -1,76 +1,7 @@
 // criando meu segundo hook
-import { useEffect, useRef, useState } from 'react';
-import './style.css';
 
-const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB);
-};
-
-const useFetch = (url, options) => {
-  const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const [shouldLoad, setShoudLoad] = useState(false);
-
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options);
-
-  useEffect(() => {
-    let changed = false;
-
-    if (!isObjectEqual(url, urlRef.current)) {
-      urlRef.current = url;
-      changed = true;
-    }
-
-    if (!isObjectEqual(options, optionsRef.current)) {
-      optionsRef.current = options;
-      changed = true;
-    }
-
-    if (changed) {
-      setShoudLoad((s) => !s);
-    }
-  }, [url, options]);
-
-  useEffect(() => {
-    let wait = false;
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    setLoading(true);
-
-    const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 1000));
-
-      try {
-        const response = await fetch(urlRef.current, {
-          signal,
-          ...optionsRef.current,
-        });
-        const jsonResult = await response.json();
-
-        if (!wait) {
-          setResult(jsonResult);
-          setLoading(false);
-        }
-      } catch (e) {
-        if (!wait) {
-          setLoading(false);
-        }
-        console.warn('ops deu erro!' + e);
-      }
-    };
-
-    fetchData();
-    return () => {
-      wait = true;
-      controller.abort();
-    };
-  }, [shouldLoad]);
-
-  return [result, loading];
-};
+import { useState } from 'react';
+import { useFetch } from './useFetch';
 
 export const Home = () => {
   const [postId, setPostId] = useState('');
